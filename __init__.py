6a07a06d0cdaa9655904ae6ea6afda30de875606
Ankluz <chistiy.random@yandex.ru>
2025-03-06 16:17:52 +0300

    Fix deleted func

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from .bbox_editor import manual_box_adjustment
 from .bbox_annotator import annotate_keypoints
 from .bbox_detector import detect_and_draw_boxes
-from .file_processor import save_annotations, load_annotations, convert_old_format_to_new
+from .file_processor import save_annotations, load_annotations
 
 __version__ = '0.1.0'
 
